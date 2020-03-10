fb3b4dbb6911cd0171696785b1afecae3b79b4a2
Philip Bower | Netmatters Ltd <philip.bower@netmatters.local>
2020-03-10 11:53:22 +0000

    sidebar html and css updated

--- JS/burger-menu.js
+++ JS/burger-menu.js
@@ -1,7 +1,17 @@
-window.onload = function() {
-	document
-		.getElementById('burger-container')
-		.addEventListener('click', function() {
-			this.classList.toggle('open');
-		});
-};
+// window.onload = function() {
+// 	document
+// 		.getElementById('burger-container')
+// 		.addEventListener('click', function() {
+// 			this.classList.toggle('open');
+// 		});
+// };
+
+const burgerMenu = document.querySelector('#burger-container');
+
+burgerMenu.addEventListener('click', e => {
+	const sidebar = document.querySelector('.nav-sidebar');
+	burgerMenu.classList.toggle('active');
+	const mainContent = document.querySelector('body');
+	sidebar.classList.toggle('active');
+	mainContent.classList.toggle('active');
+});
