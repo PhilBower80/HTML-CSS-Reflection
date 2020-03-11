ddb16c34a8877afc0a97985e9ccfc077a98ceca7
Philip Bower | Netmatters Ltd <philip.bower@netmatters.local>
2020-03-11 16:06:33 +0000

    update

--- JS/burger-menu.js
+++ JS/burger-menu.js
@@ -2,8 +2,9 @@ const burgerMenu = document.querySelector('#burger-container');
 
 burgerMenu.addEventListener('click', e => {
 	const sidebar = document.querySelector('.nav-sidebar');
-	burgerMenu.classList.toggle('active');
 	const mainContent = document.querySelector('.main-outer');
+
+	burgerMenu.classList.toggle('active');
 	sidebar.classList.toggle('active');
 	mainContent.classList.toggle('active');
 });
