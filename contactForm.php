eb349d370970724d092a41f02d5f9725a1e69c3a
Philip Bower <philbower80@outlook.com>
2020-07-22 11:54:30 +0100

    added include php files (head, header, footer, cookie). Updated href links. added $pageTitle to head. Added contactForm.php

---
+++ contactForm.php
@@ -0,0 +1,16 @@
+<?php 
+$pageTitle = 'Contact Us | Netmatters';
+include('inc/head.php'); 
+include('inc/header.php');
+?>
+
+
+<div>
+<h1>Contact Form</h1>
+</div>
+
+
+<?php 
+include('inc/footer.php'); 
+include('inc/sidebar.php'); 
+?>
---
+++ inc/cookie.php
@@ -0,0 +1,31 @@
+    <!-- COOKIE -->
+    <div class="cookie-outer">
+      <div class="brand-img">
+
+        <div class="cookie-container">
+          <div class="cookie-policy">
+            <h3>Cookies Policy</h3>
+            <p>We use cookies to obtain aggregate data regarding site traffic and interaction, in order to identify
+              user
+              trends and obtain insights in order to continually improve our site. These cookies enable us to improve
+              your
+              customer experience as you use our site and help provide you with relevant online marketing.</p>
+          </div>
+
+
+          <div class="cookie-buttons">
+            <a class="link-btn settings-btn" href="#">Change Settings <i class="fas fa-arrow-right"></i></a>
+            <a class="link-btn accept-cookie-btn" href="#">Accept Cookies</a>
+          </div>
+        </div>
+
+      </div>
+      <div class="cookie-container">
+        <p>You can see a list of the other companies who use cookies on this website, by visiting cookie settings at
+          the
+          bottom of each page. For full details of how we use your personal information, and your rights in relation
+          to
+          it, view our<a href="#"> Privacy policy</a></p>
+      </div>
+    </div> 
+    <!-- end of COOKIE -->
